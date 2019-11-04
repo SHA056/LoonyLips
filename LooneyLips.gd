19fe35993b94bf64aef3090a9a29f84b613b0e81
SHA056 <shailaja.mahara@gmail.com>
2019-11-04 21:52:01 +0530

    (add) json object

--- LooneyLips.gd
+++ LooneyLips.gd
@@ -4,26 +4,6 @@ var player_words = []
 #var story = "Detective: Hello! I'm detective %s. And you are?\nSuspect: %s\nDetective: You're here today under suspicion of second degree robbery.\nSuspect: %s!\nDetective: That's right. %s %s were stolen from %s. And the crime scene has your %s written all over it.\nSuspect: That is %s!\nDetective: Where were you on the night on %s?\nSuspect: We were watching %s!\nDetective: Then why does the security camera show you %s just %s away from the crime scene!\n\tAlright! I'm through with playing games. Where're you from?\nSuspect: %s.\nDetective: Just as I suspected. You know one of the best parts of being a detective is that I get to lock up criminals like you and go home to my children and my pet %s and say this town needs an %s.\nSuspect: Fine! I did it! I commited the robbery. But I only did it cause I needed the money to buy myself %s implants.\nDetective: I knew it all along! I knew it all along and everytime I solve a crime, I like to sing my favorite song %s.\n\t%s.\nSuspect: You have a %s voice! I love you!"
 #var prompts = ["a male name", "your favorite teacher", "an exclamation", "a number", "a plural word", "a store name", "a body part", "a silly word", "a holiday", "a movie title", "a verb ending in 'ing'", "an amount of distance", "a country", "an animal", "a famous movie quote", "another body part", "a children's song", "2 lines of that song" , "an adjective"]
 
-#var story = "The best %s in %s is an %s %s."
-#var prompts = ["a food", "a place", "an insult", "the same food"]
-
-var template = [{
-	"prompts" : ["a food", "a place", "an insult", "the same food"],
-	"story" : "The best %s in %s is an %s %s."
-	},
-	{
-	"prompts" : ["first name", "last name", "a dish"],
-	"story" : "%s %s makes the best %ss in the world."
-	},{
-	"prompts" : ["first name", "an insult", "a food", "a dish"],
-	"story" : "%s makes better %s %ses than %s."
-	},{
-	"prompts" : ["first name", "a city", "a dish"],
-	"story" : "If you ever go to %s's restaurant in %s, you should definately get %s."
-	},{
-	"prompts" : ["a dish", "an insult", "a food"],
-	"story" : "The best part about ordering %ss is that you get an %s %s for free!"
-	},]
 var current_story = {}
 
 onready var PlayerText = $VBoxContainer/HBoxContainer/PlayerText
@@ -35,23 +15,24 @@ func _ready ():
 	check_player_word_length()
 	PlayerText.grab_focus()
 
-func _on_PlayerText_text_entered(new_text):
-	if is_story_done():
-		get_tree().reload_current_scene()
-	else:
-		add_to_player_words()	
-#	PlayerText.clear()
-
-func _on_TextureButton_pressed():
-	if is_story_done():
-		get_tree().reload_current_scene()
-	else:
-		add_to_player_words()
-
-func pick_a_template():
-	randomize()
-	current_story = template[randi() % template.size()]
+func get_from_json(filename):
+	var file = File.new()
+	file.open(filename, File.READ)
+	var text = file.get_as_text()
+	var data = parse_json(text)
+	file.close()
+	return data
 	
+func pick_a_template():
+	var stories = get_from_json("storybook.json")
+	randomize()
+	current_story = stories[randi() % stories.size()]
+	
+#	var stories = $Storybook.get_child_count() 
+#	var selected_story = randi() % stories
+#	current_story.prompts = $Storybook.get_child(selected_story).prompts
+#	current_story.story = $Storybook.get_child(selected_story).story
+		
 func add_to_player_words():
 	player_words.append(PlayerText.text)
 	PlayerText.clear()
@@ -76,3 +57,17 @@ func end_game():
 	PlayerText.queue_free()
 	$VBoxContainer/HBoxContainer/Label.text = "AGAIN"
 	tell_story()
+
+func _on_PlayerText_text_entered(new_text):
+	if is_story_done():
+		get_tree().reload_current_scene()
+	else:
+		add_to_player_words()	
+#	PlayerText.clear()
+
+func _on_TextureButton_pressed():
+	if is_story_done():
+		get_tree().reload_current_scene()
+	else:
+		add_to_player_words()
+
